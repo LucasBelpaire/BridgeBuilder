c925caca209373aea2e71f508846f1ef7114b20d
Lucas Belpaire <lucas.belpaire@ugent.be>
2020-04-23 10:22:23 +0200

    implemented class which can represent the points of a bridge

---
+++ BridgeBuilder/point.py
@@ -0,0 +1,12 @@
+class Point:
+
+    def __init__(self, coordinate, is_anchored=False, neighbours=None):
+        self.coordinate = coordinate
+        self.is_anchored = is_anchored
+        if neighbours:
+            self.neighbours = neighbours
+        else:
+            self.neighbours = []
+
+    def add_neighbour(self, neighbor):
+        self.neighbours.append(neighbor)
