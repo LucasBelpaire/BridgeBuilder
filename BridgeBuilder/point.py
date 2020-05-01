00499ee0fa8b6f236a2f3f4b11a32ab4552b6f6b
Lucas Belpaire <lucas.belpaire@ugent.be>
2020-05-01 19:51:38 +0200

    code cleanup

--- BridgeBuilder/point.py
+++ BridgeBuilder/point.py
@@ -1,18 +1,15 @@
 class Point:
 
-    def __init__(self, coordinate, is_anchored_x=False, is_anchored_y=False, neighbours=None, load=0, member_weight=0):
+    def __init__(self, coordinate, is_anchored_x=False, is_anchored_y=False, load=0):
         self.coordinate = coordinate
         self.is_anchored_x = is_anchored_x
         self.is_anchored_y = is_anchored_y
-        if neighbours:
-            self.neighbours = neighbours
-        else:
-            self.neighbours = []
+        self.neighbours = []
         self.load = load
-        self.member_weight = member_weight
+        self.member_weight = 0
 
-    def add_neighbour(self, neighbor):
-        self.neighbours.append(neighbor)
+    def add_neighbour(self, neighbour):
+        self.neighbours.append(neighbour)
 
     def add_neighbours(self, neighbours):
         self.neighbours.extend(neighbours)
